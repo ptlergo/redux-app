8b8774d766674882b330252518e7129febf5743e
ptlergo <ptlergo@gmail.com>
2019-02-11 22:29:50 -0500

    actions setup with action types

---
+++ src/actions/postActions.js
@@ -0,0 +1,17 @@
+import {
+    FETCH_POSTS,
+    NEW_POST
+} from './types';
+
+
+export const fetchPosts = () => dispatch => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(posts => {
+            return dispatch({
+                type: FETCH_POSTS,
+                payload: posts
+            })
+        });
+
+}
