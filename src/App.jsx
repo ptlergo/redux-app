be3b1c13eba97ccff3e903f26c0199cc09638c7a
ptlergo <ptlergo@gmail.com>
2019-02-11 22:32:51 -0500

    refactor to use redux and import the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 import "./App.css";
 import { PostForm, Posts } from "./components";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware} from 'redux';
+
+import store from './store'
 
 export class App extends React.Component {
+
   render() {
     return (
-      <div className="App">
-        <PostForm />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <PostForm fetchPosts={this.fetchReq} />
+          <Posts fetchReq={this.fetchReq} />
+        </div>
+      </Provider>
     );
   }
 }
